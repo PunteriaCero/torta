fdab64855c498b01c4e7366e1b3258839aac602a
Luis Puerto <puertoluis6@gmail.com>
2023-08-30 15:33:00 -0300

    se muestra la seleccion

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import { data } from "./data";
 
 const handleClick = (data) => {
   console.log("Datos recibidos:", data);
+  const spanElement = document.getElementById("dataSpan");
+  spanElement.textContent = "Indice seleccionado: " + data.label;
+  spanElement.style.color = data.color;
 };
 
 function App() {
   return (
     <div className="App" style={{backgroundColor: "rgb(56, 56, 56)"}}>
       <RadarComponent data={data} onClick={handleClick} />
+      <span id="dataSpan"></span>
     </div>
   );
 }
