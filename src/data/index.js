24a4e5b7ecb0588a49cd1612d143d83a9cb63e62
Luis Puerto <puertoluis6@gmail.com>
2023-08-30 14:42:48 -0300

    quite los espacios de la data

--- src/data/index.js
+++ src/data/index.js
@@ -4,67 +4,40 @@ export const data = {
 
     {
       label: "1",
-
       startAngle: 325,
-
       endAngle: 30,
-
       innerRadius: 0.1,
-
       outerRadius: 0.9,
-
       startElevation: 4,
-
       endElevation: 6,
-
       color: "red",
-
       selected: false,
-
       value: 10,
     },
 
     {
       label: "2",
-
       startAngle: 90,
-
       endAngle: 221,
-
       innerRadius: 0,
-
       outerRadius: 0.55,
-
       startElevation: 4,
-
       endElevation: 6,
-
       color: "gray",
-
       selected: true,
-
       value: 10,
     },
 
     {
       label: "3",
-
       startAngle: 20,
-
       endAngle: 60,
-
       innerRadius: 0.3,
-
       outerRadius: 0.7,
-
       startElevation: 4,
-
       endElevation: 6,
-
       color: "orange",
-
       selected: false,
-
       value: 10,
     },    
   ],
@@ -74,51 +47,31 @@ export const data = {
 
     {
       label: "22",
-
       angle: 320,
-
       radius: 0.4,
-
       elevation: 5,
-
       color: "lightgreen",
-
       selected: false,
     }, {
       label: "8",
-
       angle: 30,
-
       radius: 0.2,
-
       elevation: 5,
-
       color: "darkcyan",
-
       selected: false,
     },{
       label: "24",
-
       angle: 200,
-
       radius: 0.9,
-
       elevation: 5,
-
       color: "green",
-
       selected: true,
     },{
       label: "0",
-
       angle: 190,
-
       radius: 0.5,
-
       elevation: 5,
-
       color: "violet",
-
       selected: false,
     }, 
   ],
