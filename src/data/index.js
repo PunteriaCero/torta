093d46cb4c60e056ed99063a3e6aa52577c241f6
Lucio Penza <lpenza@baufest.com>
2023-08-31 11:13:32 -0300

    fix: merge to main

--- src/data/index.js
+++ src/data/index.js
@@ -4,69 +4,42 @@ export const data = {
 
     {
       label: "1",
-
-      startAngle: -10,
-
+      startAngle: 325,
       endAngle: 30,
-
       innerRadius: 0.1,
-
       outerRadius: 0.9,
-
       startElevation: 4,
-
       endElevation: 6,
-
       color: "red",
-
       selected: false,
-
       value: 10,
     },
 
     {
       label: "2",
-
       startAngle: 90,
-
       endAngle: 221,
-
       innerRadius: 0,
-
       outerRadius: 0.55,
-
       startElevation: 4,
-
       endElevation: 6,
-
       color: "gray",
-
       selected: true,
-
       value: 10,
     },
 
     {
       label: "3",
-
       startAngle: 20,
-
       endAngle: 60,
-
       innerRadius: 0.3,
-
       outerRadius: 0.7,
-
       startElevation: 4,
-
       endElevation: 6,
-
       color: "orange",
-
       selected: false,
-
       value: 10,
-    },
+    },    
   ],
 
   targets: [
@@ -74,56 +47,33 @@ export const data = {
 
     {
       label: "22",
-
       angle: 320,
-
       radius: 0.4,
-
       elevation: 5,
-
       color: "lightgreen",
-
       selected: false,
-    },
-    {
+    }, {
       label: "8",
-
       angle: 30,
-
       radius: 0.2,
-
       elevation: 5,
-
       color: "darkcyan",
-
       selected: false,
-    },
-    {
+    },{
       label: "24",
-
       angle: 200,
-
       radius: 0.9,
-
       elevation: 5,
-
       color: "green",
-
-      selected: true,
-    },
-    {
-      label: "0",
-
-      angle: 190,
-
-      radius: 0.5,
-
-      elevation: 5,
-
-      color: "violet",
-
       selected: false,
-    },
+    },{
+      label: "0",
+      angle: 190,
+      radius: 0.5,
+      elevation: 5,
+      color: "violet",
+      selected: false,
+    }, 
   ],
 };
 
@@ -133,88 +83,49 @@ export const dataDos = {
 
     {
       label: "3",
-
-      startAngle: 100,
-
+      startAngle: 93,
       endAngle: 105,
-
       innerRadius: 0.03,
-
       outerRadius: 0.9,
-
       startElevation: 4,
-
       endElevation: 6,
-
       color: "violet",
-
       selected: false,
-
       value: 10,
-    },
-    {
+    } ,{
       label: "8",
-
       startAngle: 9,
-
       endAngle: 78,
-
       innerRadius: 0.2,
-
       outerRadius: 0.3,
-
       startElevation: 4,
-
       endElevation: 6,
-
       color: "yellow",
-
       selected: false,
-
       value: 10,
-    },
-    {
+    }  ,{
       label: "4",
-
       startAngle: 129,
-
       endAngle: 298,
-
       innerRadius: 0.1,
-
       outerRadius: 0.75,
-
       startElevation: 4,
-
       endElevation: 6,
-
       color: "darkcyan",
-
       selected: false,
-
       value: 10,
-    },
-    {
+    }  ,{
       label: "5",
-
       startAngle: 250,
-
       endAngle: 15,
-
       innerRadius: 0.4,
-
-      outerRadius: 0.7,
-
+      outerRadius: 0.70,
       startElevation: 4,
-
       endElevation: 6,
-
       color: "lightblue",
-
       selected: false,
-
       value: 10,
-    },
+    }  
   ],
 
   targets: [
@@ -222,29 +133,18 @@ export const dataDos = {
 
     {
       label: "7",
-
       angle: 367,
-
       radius: 0.89,
-
       elevation: 5,
-
       color: "purple",
-
       selected: false,
-    },
-    {
+    }, {
       label: "1",
-
       angle: 340,
-
       radius: 0.3,
-
       elevation: 5,
-
       color: "orange",
-
       selected: false,
-    },
+    }
   ],
 };
