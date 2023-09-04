711a04ded12cfa5713308c56ee12d52a33114edd
lpenza <105391483+lpenza@users.noreply.github.com>
2023-09-04 12:31:01 -0300

    Fix/useeffect/lp (#8)
    * fix: eslint warning
    
    * fix: lines
    
    ---------
    
    Co-authored-by: Lucio Penza <lpenza@baufest.com>

--- src/components/refRadarComponent/utils/index.js
+++ src/components/refRadarComponent/utils/index.js
@@ -1,4 +1,4 @@
-export function generateBaseCircles(numCircles, color) {
+function generateBaseCircles(numCircles, color) {
   const baseCircles = [];
 
   for (let i = 0; i < numCircles; i++) {
@@ -9,7 +9,14 @@ export function generateBaseCircles(numCircles, color) {
   return baseCircles;
 }
 
-export function BaseCircles({ svg, radius, circleStroke, baseCircles }) {
+export function BaseCircles({
+  svg,
+  numCircles,
+  colorCircles,
+  radius,
+  circleStroke,
+}) {
+  const baseCircles = generateBaseCircles(numCircles, colorCircles);
   const base = svg.append("g").attr("class", "base");
 
   baseCircles.forEach((circle) => {
@@ -17,10 +24,28 @@ export function BaseCircles({ svg, radius, circleStroke, baseCircles }) {
       .append("circle")
       .attr("cx", 0)
       .attr("cy", 0)
-      .attr("r", circle.radius * (radius-3))
+      .attr("r", circle.radius * (radius - 3))
       .attr("stroke", circle.color)
       .attr("stroke-width", circleStroke)
       .attr("fill", "none");
+  });
+}
+
+export function BaseLines({ lineAngles,radius,colorLines,strokeLines,opacityLines, svg }) {
+  // Dibujar líneas desde el centro hasta el radio especificado
+  lineAngles.forEach((angle) => {
+    const x2 = Math.cos(angle) * radius;
+    const y2 = Math.sin(angle) * (-radius + 1.5);
+
+    svg
+      .append("line")
+      .attr("x1", 0)
+      .attr("y1", 0)
+      .attr("x2", x2)
+      .attr("y2", y2)
+      .attr("stroke", colorLines)
+      .attr("stroke-width", strokeLines)
+      .attr("opacity", opacityLines);
   });
 }
 
