9fe4391fe36bbdcafb6c2a4b5d215260f3ea3437
Lucio Penza <lpenza@baufest.com>
2023-09-04 18:49:56 -0300

    fix: delete useeffect

--- src/components/refRadarComponent/hooks/useRadarComponent/index.js
+++ src/components/refRadarComponent/hooks/useRadarComponent/index.js
@@ -1,71 +1,70 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { compareByEndElevation } from "../../utils";
 
-export const useRadarComponent = ({data, onClick ,config: {
-  radius = 200,
-  numCircles = 9,
-  colorCircles = "green",
-  numLines = 24,
-  opacityLines = 0.3,
-  strokeLines = 1,
-  colorLines = "green",
-  circleStroke = 3,
-  north = "N",
-  northColor = "rgb(9, 115, 9)",
-  northFontSize = (radius) => radius * 0.06,
-  opacity = 0.4,
-  sectionLabelFontSize = (radius) => radius * 0.06,
-  sectionLabelFontWeight = "bold",
-  sectionLabelDefaultColor = "rgb(100, 100, 100)",
-  sectionLabelSelectedColor = "whitesmoke",
-  selectedSectiondropShadowFilter = "drop-shadow(0px 1.5px 1.5px rgba(0, 0, 0, 0.6))",
-  unSelectedSectiondropShadowFilter = "drop-shadow(0px 1.5px 1.5px rgba(0, 0, 0, 0.5))",
-  sectionRectWidth = (radius) => radius * 0.08,
-  sectionRectHeight = (radius) => radius * 0.08,
-  sectionBorderStroke = (radius) => radius * 0.01,
-  sectionStrokeColor = "white",
-  sectionRecBorderSrtoke = (radius) => radius * 0.005,
-  unselecteSectionRecColor = "white",
-  selectedSectionRecBorderColor = "black",
-  unselectedSectionRecBorderColor = "black",
-  unSelectedSectionLabelShadow = "drop-shadow(0px 0px 0.7px rgba(0, 0, 0, 1))",
-  pointLabelFontSize = (radius) => radius * 0.06,
-  pointLabelFontWeight = "bold",
-  pointLabelTextColor = "whitesmoke",
-  pointLabelTextShadow = "0 0 1px black, 0 0 1px black",
-  pointRectWidth = (radius) => radius * 0.04,
-  pointRectHeight = (radius) => radius * 0.04,
-  pointBorderStroke = 1.4,
-  selectedPointRectborderShadow = (color) =>
-    `drop-shadow(0px 0px 3px ${color})`,
-  unSelectedPointRectborderShadow = "drop-shadow(0px 1.5px 1.5px rgba(0, 0, 0, 0.5))",
-  selectedPointStrokeColor = "white",
-  pointRectRx = (radius) => radius * 0.08,
-  pointRectRy = (radius) => radius * 0.08,
-}}) => {
+export const useRadarComponent = ({
+  data,
+  onClick,
+  config: {
+    radius = 200,
+    numCircles = 9,
+    colorCircles = "green",
+    numLines = 24,
+    opacityLines = 0.3,
+    strokeLines = 1,
+    colorLines = "green",
+    circleStroke = 3,
+    north = "N",
+    northColor = "rgb(9, 115, 9)",
+    northFontSize = (radius) => radius * 0.06,
+    opacity = 0.4,
+    sectionLabelFontSize = (radius) => radius * 0.06,
+    sectionLabelFontWeight = "bold",
+    sectionLabelDefaultColor = "rgb(100, 100, 100)",
+    sectionLabelSelectedColor = "whitesmoke",
+    selectedSectiondropShadowFilter = "drop-shadow(0px 1.5px 1.5px rgba(0, 0, 0, 0.6))",
+    unSelectedSectiondropShadowFilter = "drop-shadow(0px 1.5px 1.5px rgba(0, 0, 0, 0.5))",
+    sectionRectWidth = (radius) => radius * 0.08,
+    sectionRectHeight = (radius) => radius * 0.08,
+    sectionBorderStroke = (radius) => radius * 0.01,
+    sectionStrokeColor = "white",
+    sectionRecBorderSrtoke = (radius) => radius * 0.005,
+    unselecteSectionRecColor = "white",
+    selectedSectionRecBorderColor = "black",
+    unselectedSectionRecBorderColor = "black",
+    unSelectedSectionLabelShadow = "drop-shadow(0px 0px 0.7px rgba(0, 0, 0, 1))",
+    pointLabelFontSize = (radius) => radius * 0.06,
+    pointLabelFontWeight = "bold",
+    pointLabelTextColor = "whitesmoke",
+    pointLabelTextShadow = "0 0 1px black, 0 0 1px black",
+    pointRectWidth = (radius) => radius * 0.04,
+    pointRectHeight = (radius) => radius * 0.04,
+    pointBorderStroke = 1.4,
+    selectedPointRectborderShadow = (color) =>
+      `drop-shadow(0px 0px 3px ${color})`,
+    unSelectedPointRectborderShadow = "drop-shadow(0px 1.5px 1.5px rgba(0, 0, 0, 0.5))",
+    selectedPointStrokeColor = "white",
+    pointRectRx = (radius) => radius * 0.08,
+    pointRectRy = (radius) => radius * 0.08,
+  },
+}) => {
   const [sectionsData, setSectionsData] = useState(data.sections);
   const [targetsData, settTargetsData] = useState(data.targets);
   const width = radius * 2;
   const height = width;
 
-  useEffect(()=>{
-    if(sectionsData){
-      sectionsData.sort(compareByEndElevation);
-    }
-  }, [sectionsData])
-
-  useEffect(()=>{
-    if(targetsData){
-      targetsData.sort(compareByEndElevation);
-    }
-  }, [targetsData])
+  if (sectionsData) {
+    sectionsData.sort(compareByEndElevation);
+  }
+  if (targetsData) {
+    targetsData.sort(compareByEndElevation);
+  }
 
   const updateSelectedState = (dataArray, label) =>
     dataArray.map((data) => ({ ...data, selected: data.label === label }));
 
   const handleSectionClick = (event, d) => {
     const newSectionsData = updateSelectedState(sectionsData, d.data.label);
-    if(targetsData){
+    if (targetsData) {
       const newTargetsData = updateSelectedState(targetsData, null); // Unselect all targets
       settTargetsData(newTargetsData);
     }
@@ -76,7 +75,7 @@ export const useRadarComponent = ({data, onClick ,config: {
 
   const handleTargetsClick = (event, d) => {
     const newTargetsData = updateSelectedState(targetsData, d.data.label);
-    if(sectionsData){
+    if (sectionsData) {
       const newSectionsData = updateSelectedState(sectionsData, null); // Unselect all sections
       setSectionsData(newSectionsData);
     }
@@ -92,46 +91,46 @@ export const useRadarComponent = ({data, onClick ,config: {
     sectionsData,
     width,
     height,
-    radius ,
+    radius,
     numCircles,
-    colorCircles ,
+    colorCircles,
     numLines,
-    opacityLines ,
-    strokeLines ,
-    colorLines ,
-    circleStroke ,
-    north ,
-    northColor ,
-    northFontSize ,
+    opacityLines,
+    strokeLines,
+    colorLines,
+    circleStroke,
+    north,
+    northColor,
+    northFontSize,
     opacity,
 
-    sectionLabelFontSize ,
-    sectionLabelFontWeight ,
-    sectionLabelDefaultColor ,
-    sectionLabelSelectedColor ,
-    selectedSectiondropShadowFilter ,
+    sectionLabelFontSize,
+    sectionLabelFontWeight,
+    sectionLabelDefaultColor,
+    sectionLabelSelectedColor,
+    selectedSectiondropShadowFilter,
     unSelectedSectiondropShadowFilter,
-    sectionRectWidth ,
-    sectionRectHeight ,
-    sectionBorderStroke ,
-    sectionStrokeColor ,
-    sectionRecBorderSrtoke ,
-    unselecteSectionRecColor ,
-    selectedSectionRecBorderColor ,
-    unselectedSectionRecBorderColor ,
-    unSelectedSectionLabelShadow ,
+    sectionRectWidth,
+    sectionRectHeight,
+    sectionBorderStroke,
+    sectionStrokeColor,
+    sectionRecBorderSrtoke,
+    unselecteSectionRecColor,
+    selectedSectionRecBorderColor,
+    unselectedSectionRecBorderColor,
+    unSelectedSectionLabelShadow,
 
-    pointLabelFontSize ,
-    pointLabelFontWeight ,
-    pointLabelTextColor ,
-    pointLabelTextShadow ,
-    pointRectWidth ,
+    pointLabelFontSize,
+    pointLabelFontWeight,
+    pointLabelTextColor,
+    pointLabelTextShadow,
+    pointRectWidth,
     pointRectHeight,
-    pointBorderStroke ,
-    selectedPointRectborderShadow ,
-    unSelectedPointRectborderShadow ,
-    selectedPointStrokeColor ,
-    pointRectRx ,
+    pointBorderStroke,
+    selectedPointRectborderShadow,
+    unSelectedPointRectborderShadow,
+    selectedPointStrokeColor,
+    pointRectRx,
     pointRectRy,
   };
 };
--- src/data/index.js
+++ src/data/index.js
@@ -8,6 +8,7 @@ export const data = {
       innerRadius: 0.1,
       outerRadius: 0.9,
       startElevation: 4,
+      endElevation:100,
       color: "red",
       selected: false,
       value: 10,
@@ -20,6 +21,7 @@ export const data = {
       innerRadius: 0,
       outerRadius: 0.55,
       startElevation: 4,
+      endElevation:1,
       color: "gray",
       selected: false,
       value: 10,
@@ -32,6 +34,7 @@ export const data = {
       innerRadius: 0.3,
       outerRadius: 0.7,
       startElevation: 4,
+      endElevation:2,
       color: "orange",
       selected: true,
       value: 10,
@@ -40,80 +43,12 @@ export const data = {
 
   targets: [
     //   // {label, angle, radius, elevation, color, selected},
-    //   {
-    //     label: "22",
-    //     angle: 320,
-    //     radius: 0.8,
-    //     elevation: 5,
-    //     color: "lightgreen",
-    //     selected: false,
-    //   }, {
-    //     label: "8",
-    //     angle: 90,
-    //     radius: 0.2,
-    //     elevation: 5,
-    //     color: "darkcyan",
-    //     selected: false,
-    //   },{
-    //     label: "24",
-    //     angle: 200,
-    //     radius: 0.9,
-    //     elevation: 5,
-    //     color: "yellow",
-    //     selected: false,
-    //   }
   ],
 };
 
 export const dataDos = {
   sections: [
     // {label, startAngle, endAngle, innerRadius, outerRadius, startElevation, endElevation, color, selected},
-    // {
-    //   label: "3",
-    //   startAngle: 93,
-    //   endAngle: 105,
-    //   innerRadius: 0.03,
-    //   outerRadius: 0.9,
-    //   startElevation: 4,
-    //   endElevation: 6,
-    //   color: "violet",
-    //   selected: false,
-    //   value: 10,
-    // }
-    //  ,{
-    //   label: "8",
-    //   startAngle: 9,
-    //   endAngle: 78,
-    //   innerRadius: 0.2,
-    //   outerRadius: 0.3,
-    //   startElevation: 4,
-    //   endElevation: 6,
-    //   color: "yellow",
-    //   selected: false,
-    //   value: 10,
-    // }  ,{
-    //   label: "4",
-    //   startAngle: 129,
-    //   endAngle: 298,
-    //   innerRadius: 0.1,
-    //   outerRadius: 0.75,
-    //   startElevation: 4,
-    //   endElevation: 6,
-    //   color: "darkcyan",
-    //   selected: false,
-    //   value: 10,
-    // }  ,{
-    //   label: "5",
-    //   startAngle: 250,
-    //   endAngle: 15,
-    //   innerRadius: 0.4,
-    //   outerRadius: 0.70,
-    //   startElevation: 4,
-    //   endElevation: 6,
-    //   color: "lightblue",
-    //   selected: false,
-    //   value: 10,
-    // }
   ],
   targets: [
     // {label, angle, radius, elevation, color, selected},
